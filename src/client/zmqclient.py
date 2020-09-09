ed6daceeca3b5a822c4a2968aa7a9341db651738
Alejandro Sosa <alejandro.sosa@incluit.com>
2020-09-09 18:24:56 -0300

    added aws logic

--- src/client/zmqclient.py
+++ src/client/zmqclient.py
@@ -8,8 +8,7 @@ import os
 HOST = '127.0.0.1'
 PORT = '4444'
 
-logging.basicConfig(filename='logs/subscriber.log', level=logging.INFO)
-
+logging.basicConfig(filename='../../logs/subscriber.log', level=logging.INFO)
 
 class ZClient(object):
 
@@ -21,17 +20,62 @@ class ZClient(object):
         self._subscriber = self._context.socket(zmq.SUB)
         print("Client Initiated")
 
-    def receive_message(self):
+    def receive_message(self,count = 1):
         """Start receiving messages"""
         self._subscriber.connect('tcp://{}:{}'.format(self.host, self.port))
         self._subscriber.setsockopt(zmq.SUBSCRIBE, b"")
 
         while True:
             print('listening on tcp://{}:{}'.format(self.host, self.port))
-            message = self._subscriber.recv()
+            message = self._subscriber.recv().decode("utf-8")
             print(message)
             logging.info(
                 '{}   - {}'.format(message, time.strftime("%Y-%m-%d %H:%M")))
+            count +=1
+            Esearch().hit_kibana(message, count)
+
+
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+import boto3
+
+regionaws = 'us-east-1'  # e.g. us-west-1
+hostaws = 'search-driver-management-2lzumo4geewhdzwcxsqnoacrmy.us-east-1.es.amazonaws.com'  # For example, my-test-domain.us-east-1.es.amazonaws.com
+
+
+class Esearch(object):
+
+    @staticmethod
+    def hit_kibana(message, count):
+        service = 'es'
+        credentials = boto3.Session().get_credentials()
+        awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, regionaws, service,
+                           session_token=credentials.token)
+        l = message.split(",");
+
+        es = Elasticsearch(
+            index='alerts',
+            hosts=[{'host': hostaws, 'port': 443}],
+            http_auth=awsauth,
+            use_ssl=True,
+            verify_certs=True,
+            connection_class=RequestsHttpConnection
+        )
+
+        document = {
+            "service": l[0],
+            "date": l[1],
+            "time": l[2],
+            "camera": l[3],
+            "event": l[4],
+            "precision": l[5]
+        }
+
+        count +=1
+
+        es.index(index="alerts", doc_type="_doc", id=count, body=document)
+
+        print(es.get(index="alerts", doc_type="_doc", id=count))
 
 
 if __name__ == '__main__':
